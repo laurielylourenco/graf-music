db0944723fee738935c821c27eaf161665c8203f
lauriely lourenço <49760702+laurielylourenco@users.noreply.github.com>
2023-10-17 20:34:25 -0300

    Atualizar o page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
 
+import './App.css';
 export default function Home() {
   return (
 
-        <p>
-          Vamos lá
+    <div className="landing-page">
+      <nav className="navbar">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Sobre</a></li>
+        </ul>
+      </nav>
+      <header>
+        <h1>Bem-vindo ao Mundo da Música</h1>
+        <button>Login</button>
+      </header>
+    </div>
 
-        </p>
-      
   )
 }
